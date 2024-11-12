4041e70f030c5b2345e9a62a33175d7e7bb078d3
sownt <thonsai@outlook.com>
2024-11-12 14:29:03 +0700

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -9,10 +9,15 @@ export async function middleware(request: NextRequest) {
     if (isAdminRoute && !sessionCookie) {
         const url = request.nextUrl.clone();
         url.pathname = '/login';
+        url.searchParams.set('redirect', request.nextUrl.href);
         return NextResponse.redirect(url);
     }
 
     if (isLoginRoute && sessionCookie) {
+        const redirectUrl = request.nextUrl.searchParams.get('redirect');
+        if (redirectUrl) {
+            return NextResponse.redirect(redirectUrl)
+        }
         const url = request.nextUrl.clone();
         url.pathname = '/admin/dashboard';
         return NextResponse.redirect(url);
