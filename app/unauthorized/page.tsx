8af8e9955010abf5ec9fdfc0eeb77b456bdf79f1
sownt <thonsai@outlook.com>
2024-11-27 13:49:22 +0700

    update ui

--- app/unauthorized/page.tsx
+++ app/unauthorized/page.tsx
@@ -1,6 +1,6 @@
 export default function Unauthorized() {
     return (
-        <div className="flex w-full h-screen items-center justify-center">
+        <div className="flex w-full h-screen items-center justify-center p-8">
             Bạn chưa được cấp quyền truy cập tính năng này. Vui lòng liên hệ quản trị viên.
         </div>
     );
